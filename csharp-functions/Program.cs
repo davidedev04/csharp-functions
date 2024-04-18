b399721c90c7b1eae46cb89acaf3b7f039905750
Davide Prencipe <davidedev04@gmail.com>
2024-04-18 14:38:00 +0200

    - checkpoint: aggiunte funzioni richieste e stampato in console l'array di numeri, i quadrati di tutti i numeri, e l'array con tutti i numeri al quadrato

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace csharp_functions
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace csharp_functions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            void StampaArray(int[] array)
+            {
+                Console.Write("( ");
+                
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+
+                Console.WriteLine(" )");
+            }
+
+            int Quadrato(int numero)
+            {
+                int quadratoNumber = numero * numero;
+
+                Console.WriteLine($"il quadrato del numero {numero} é {quadratoNumber}");
+                return numero;
+            }
+     
+            int[] ElevaAlQuadrato(int[] array)
+            {
+                
+                Console.Write("( ");
+
+                for (int i = 0;i < array.Length;i++)
+                    {
+                        int quadratoNumber = array[i] * array[i];
+                        Console.Write(quadratoNumber);
+
+                        if (i < array.Length - 1)
+                        {
+                            Console.Write(", ");
+                        }
+                    }
+
+                Console.Write(" )");
+
+                return array;
+            }
+
+            int[] arrayNum = { 2, 6, 7, 5, 3, 9 };
+            
+            StampaArray(arrayNum);
+
+            for (int i = 0; i < arrayNum.Length; i++)
+            {
+                Quadrato(arrayNum[i]);
+            }
+
+            ElevaAlQuadrato(arrayNum);
         }
     }
 }
